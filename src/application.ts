62544e30776e5532960e25a8be13bf58e2e55200
Abhishek Lal <abhisheklalnediya@gmail.com>
2019-08-12 14:10:55 +0530

    explorer path

--- src/application.ts
+++ src/application.ts
@@ -43,7 +43,7 @@ export class RmCommentsServerApplication extends BootMixin(
 
     // Customize @loopback/rest-explorer configuration here
     this.bind(RestExplorerBindings.CONFIG).to({
-      path: '/api/explorer',
+      path: '/explorer',
     });
     this.component(RestExplorerComponent);
 
